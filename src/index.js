9ffd6bf4eb3784c5704ecde67a307dbde4ed53d0
Uellington Damasceno <uellington99@gmail.com>
2020-01-21 18:00:11 -0300

    Add procfile and cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express'); 
+const cors = require('cors');
 const routes = require('./routes');
 
 const PORT = 3000;
 
 const app = express();
+app.use(cors());
 app.use(routes);
 
-app.listen(PORT, () =>{
+app.listen(process.env.PORT || PORT, () =>{
   console.log("The server is running!");
 });
