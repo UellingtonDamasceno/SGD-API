efab676a2aa2233e4e74191eaff7b0a2eef06842
Uellington Damasceno <uellington99@gmail.com>
2020-02-27 10:33:53 -0300

    Complete e-mail funcion

--- src/routes.js
+++ src/routes.js
@@ -8,118 +8,92 @@ const Auth = require('./auth/Auth');
 const Passport = require('./auth/Passport')
 const { ROLES } = require('./auth/Roles')
 const Utils = require('./auth/Utils')
+// const School = require('./controllers/SchoolController');
+// const User = require('./controllers/UserController');
+// const authSchool = require('./auth/AuthSchool');
+// const passportSchool = require('./auth/PassportSchool')();
+// const Visitor = require("./controllers/VisitorController");
 const routes = Router();
+const correio = require("./services/mail/email");
 
 Passport.initialize()
 
 routes.get("/", (request, response) => {
+  correio.sendMail("uellington99@gmail.com", "Teste nodejs", "Sucesso!");
   response.json({
       status: "connected",
       message: "Hello world"
   });
 });
 
-routes.post("/validateSchoolToken",  (request, response) => {
-  Auth.validateToken(request, response)
-});
+// routes.post("/validateSchoolToken",  (request, response) => {
+//   authSchool.validateToken(request, response)
+// });
 
-routes.post("/adicionarEscola", (request, response) => {
-  School.addNewSchool(request, response);
-});
+// routes.post("/adicionarEscola", (request, response) => {
+//   School.addNewSchool(request, response);
+// });
+ 
+// routes.post("/entrarEscola", (request, response) => {
+//   authSchool.signIn(request, response);
+// }); 
+
+// routes.post("/autenticaUsuario", (request, response) => {
+//   User.getUserByLogin(request, response, result => {
+//     let status;
+//     if(!result){
+//       status = "Usuário não existe!";
+//     }else if(!(result.Senha === request.body.senha)){
+//       status = "Senha não confere, tente novamente!";
+//     }else{
+//       response.sendStatus(200);
+//     }
+
+//     response.json({
+//       status: status
+//     });
+//   });
+// });
+
+// routes.post("/autenticaEscola", (request, response) => {
+//     School.getSchoolByLogin(request, response, result => {
+//       let status;
+//       if(!result){
+//         status = "Escola não existe!";
+//       }else if(!(result.Senha === request.body.senha)){
+//         status = "Senha não confere, tente novamente!";
+//       }else{
+//         response.sendStatus(200);
+//       }
   
-routes.post("/entrarEscola", (request, response) => {
-  Auth.signIn(request, response);
-}); 
+//       response.json({
+//         status: status
+//       });
 
-routes.post("/login", (request, response) => {
-  //
-})
+//     });
+// });
 
-routes.post("/autenticaUsuario", (request, response) => {
-  User.getUserByLogin(request, response, result => {
-    let status;
-    if(!result){
-      status = "Usuário não existe!";
-    }else if(!(result.Senha === request.body.senha)){
-      status = "Senha não confere, tente novamente!";
-    }else{
-      response.sendStatus(200);
-    }
+// routes.post("/adicionarVisitante", (request, response) =>{
+//   Visitor.addNewVisitor(request, response, result =>{
+//     if(result){
+//       response.sendStatus(200);
+//     }else{
+//       response.json({
+//         status: "Erro ao cadastrar."
+//       });
+//     }
+//   });
+// });
 
-    response.json({
-      status: status
-    });
-  });
-});
-
-
-// Isso nunca vai funcionar, usa o método de Auth
-routes.post("/autenticaEscola", (request, response) => {
-    School.getSchoolByLogin(request, response, result => {
-      let status;
-      if(!result){
-        status = "Escola não existe!";
-      }else if(!(result.Senha === request.body.senha)){
-        status = "Senha não confere, tente novamente!";
-      }else{
-        response.sendStatus(200);
-      }
-  
-      response.json({
-        status: status
-      });
-
-    });
-});
-
-routes.post("/adicionarVisitante", (request, response) =>{
-  Visitor.addNewVisitor(request, response, result =>{
-    if(result){
-      response.sendStatus(200);
-    }else{
-      response.json({
-        status: "Erro ao cadastrar."
-      });
-    }
-  });
-});
-
-routes.get("/escolaPerfil", 
-  Passport.authenticate(), 
-  Utils.checkIsInRole(ROLES.School),
-  (request, response) => {
-    response.json({
-      worked: true,
-      type: 'School'
-    });
-});
+// routes.get("/escolaPerfil", passportSchool.authenticate(), (request, response) => {
+//   response.json({
+//     worked: true
+//   });
+// });
 
 routes.post("/authUser", (request, response) => {
   Auth.signIn(request, response)
 });
-
-
-routes.get("/userPerfil", Passport.authenticate(), (request, response) => {
-  response.json({
-    worked: true,
-    type: 'User'
-  });
-});
- 
-routes.post("/adicionarBolsista", (request, response) => {
-  Scholarship.addNewScholarship(request, response);
-});
-
-routes.get("/scholarshipPerfil", 
-  Passport.authenticate(),
-  Utils.checkIsInRole(ROLES.Scholarship),
-  (request, response) => {
-    response.json({
-      worked: true,
-      type: 'Scholarship'
-    });
-  }
-)
 
 routes.post("/adicionarFuncionario", (request, response) => {
   Employee.addNewEmployee(request, response)
