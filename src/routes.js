2d60929ab8a8a7451d1adadc2449f71bcb3052d5
Uellington Damasceno <uellington99@gmail.com>
2020-02-27 22:42:25 -0300

    Complete e-mail feature

--- src/routes.js
+++ src/routes.js
@@ -1,84 +1,83 @@
 const { Router } = require('express');
-// const School = require('./controllers/SchoolController');
-// const User = require('./controllers/UserController');
-// const authSchool = require('./auth/AuthSchool');
-// const passportSchool = require('./auth/PassportSchool')();
-// const Visitor = require("./controllers/VisitorController");
+const School = require('./controllers/SchoolController');
+const User = require('./controllers/UserController');
+const authSchool = require('./auth/AuthSchool');
+const passportSchool = require('./auth/PassportSchool')();
+const Visitor = require("./controllers/VisitorController");
 const routes = Router();
 const correio = require("./services/mail/email");
 
 routes.get("/", (request, response) => {
-  correio.sendMail("uellington99@gmail.com", "Teste nodejs", "Sucesso!");
   response.json({
       status: "connected",
       message: "Hello world"
   });
 });
 
-// routes.post("/validateSchoolToken",  (request, response) => {
-//   authSchool.validateToken(request, response)
-// });
+routes.post("/validateSchoolToken",  (request, response) => {
+  authSchool.validateToken(request, response)
+});
 
-// routes.post("/adicionarEscola", (request, response) => {
-//   School.addNewSchool(request, response);
-// });
+routes.post("/adicionarEscola", (request, response) => {
+  School.addNewSchool(request, response);
+});
  
-// routes.post("/entrarEscola", (request, response) => {
-//   authSchool.signIn(request, response);
-// }); 
+routes.post("/entrarEscola", (request, response) => {
+  authSchool.signIn(request, response);
+}); 
 
-// routes.post("/autenticaUsuario", (request, response) => {
-//   User.getUserByLogin(request, response, result => {
-//     let status;
-//     if(!result){
-//       status = "Usuário não existe!";
-//     }else if(!(result.Senha === request.body.senha)){
-//       status = "Senha não confere, tente novamente!";
-//     }else{
-//       response.sendStatus(200);
-//     }
+routes.post("/autenticaUsuario", (request, response) => {
+  User.getUserByLogin(request, response, result => {
+    let status;
+    if(!result){
+      status = "Usuário não existe!";
+    }else if(!(result.Senha === request.body.senha)){
+      status = "Senha não confere, tente novamente!";
+    }else{
+      response.sendStatus(200);
+    }
 
-//     response.json({
-//       status: status
-//     });
-//   });
-// });
+    response.json({
+      status: status
+    });
+  });
+});
 
-// routes.post("/autenticaEscola", (request, response) => {
-//     School.getSchoolByLogin(request, response, result => {
-//       let status;
-//       if(!result){
-//         status = "Escola não existe!";
-//       }else if(!(result.Senha === request.body.senha)){
-//         status = "Senha não confere, tente novamente!";
-//       }else{
-//         response.sendStatus(200);
-//       }
+routes.post("/autenticaEscola", (request, response) => {
+    School.getSchoolByLogin(request, response, result => {
+      let status;
+      if(!result){
+        status = "Escola não existe!";
+      }else if(!(result.Senha === request.body.senha)){
+        status = "Senha não confere, tente novamente!";
+      }else{
+        response.sendStatus(200);
+      }
   
-//       response.json({
-//         status: status
-//       });
+      response.json({
+        status: status
+      });
 
-//     });
-// });
+    });
+});
 
-// routes.post("/adicionarVisitante", (request, response) =>{
-//   Visitor.addNewVisitor(request, response, result =>{
-//     if(result){
-//       response.sendStatus(200);
-//     }else{
-//       response.json({
-//         status: "Erro ao cadastrar."
-//       });
-//     }
-//   });
-// });
+routes.post("/adicionarVisitante", (request, response) =>{
+  Visitor.addNewVisitor(request, response, result =>{
+    if(result){
+      response.sendStatus(200);
+    }else{
+      response.json({
+        status: "Erro ao cadastrar."
+      });
+    }
+  });
+});
 
-// routes.get("/escolaPerfil", passportSchool.authenticate(), (request, response) => {
-//   response.json({
-//     worked: true
-//   });
-// });
+routes.get("/escolaPerfil", passportSchool.authenticate(), (request, response) => {
+  response.json({
+    worked: true
+  });
+});
 
 
 module.exports = routes;
--- src/services/database/connection.js
+++ src/services/database/connection.js
@@ -11,9 +11,9 @@ const connection = mySql.createConnection({
 
 mySql.createConnection({multipleStatements: true});
 
- connection.connect(function(err) {
-     if (err) throw err;
-     console.log("Connected!");
- });
+//  connection.connect(function(err) {
+//      if (err) throw err;
+//      console.log("Connected!");
+//  });
 
 module.exports = connection;
