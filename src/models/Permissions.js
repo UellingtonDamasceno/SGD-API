ca79ba457c019c88d9ffa3c9e72a98c6ad9e6e10
GustavoMB <36745850+GusavoMB@users.noreply.github.com>
2020-03-07 22:14:09 -0300

    Correção banco fatal

--- src/models/Permissions.js
+++ src/models/Permissions.js
@@ -1,106 +1,155 @@
-const connection = require("../services/database/connection");
+const pool = require("../services/database/connection");
 
 exports.add = function add(idFuncionario, gerirBolsista, gerirFuncionario, validarAgendamentos, confirmarVisita, gerarRelatorio, inserirAtividade, cadastrarAtracao, callback){
-    var sql = "INSERT INTO permissoes (idFuncionario, gerirBolsista, gerirFuncionario, validarAgendamento, confirmarVisita, gerarRelatorio, inserirAtividades, cadastrarAtracao) VALUES  ?"
-    var values = [[idFuncionario, gerirBolsista, gerirFuncionario, validarAgendamentos, confirmarVisita, gerarRelatorio, inserirAtividade, cadastrarAtracao]]
-    connection.query(sql, [values], function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
+        var sql = "INSERT INTO permissoes (idFuncionario, gerirBolsista, gerirFuncionario, validarAgendamento, confirmarVisita, gerarRelatorio, inserirAtividades, cadastrarAtracao) VALUES  ?"
+        var values = [[idFuncionario, gerirBolsista, gerirFuncionario, validarAgendamentos, confirmarVisita, gerarRelatorio, inserirAtividade, cadastrarAtracao]]
+        connection.query(sql, [values], function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });
     });
 }
 
 exports.getPermissoes = function getPermissoes(callback){
-    var sql = "SELECT * FROM permissoes"
-    connection.query(sql, function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
+        var sql = "SELECT * FROM permissoes"
+        connection.query(sql, function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });
     });
 }
 
 exports.getByIdPermissoes = function getByIdPermissoes(idPerrmissoes, callback){
-    var sql = "SELECT * FROM permissoes WHERE idPermissoes = ?"
-    connection.query(sql, idPerrmissoes, function(err, result){
-        if(err) throw err;
-        callback(result);
+    pool.getConnection(function(err, connection){
+        if (err) throw err;
+        var sql = "SELECT * FROM permissoes WHERE idPermissoes = ?"
+        connection.query(sql, idPerrmissoes, function(err, result){
+            if(err) throw err;
+            callback(result);
+            connection.release();
+        });
     });
 }
 
 exports.getByIdFuncionario = function getByIdFuncionario(idFuncionario, callback){
-    var sql = "SELECT * FROM permissoes WHERE idFuncionario = ?"
-    connection.query(sql, idFuncionario, function(err, result){
-        if(err) throw err;
-        callback(result);
+    pool.getConnection(function(err, connection){
+        if (err) throw err;
+        var sql = "SELECT * FROM permissoes WHERE idFuncionario = ?"
+        connection.query(sql, idFuncionario, function(err, result){
+            if(err) throw err;
+            callback(result);
+            connection.release();
+        });
     });
 }
 
 exports.setBolsista = function setBolsista(idPermissoes, boolean, callback){
-    var sql ="UPDATE permissoes SET gerirBolsista = ? WHERE idPermissoes = ?"
-    var values = [boolean, idPermissoes]
-    connection.query(sql, [values], function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
+        var sql ="UPDATE permissoes SET gerirBolsista = ? WHERE idPermissoes = ?"
+        var values = [boolean, idPermissoes]
+        connection.query(sql, [values], function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });
     })
 }
+
 exports.setFuncionarios = function setFuncionarios(idPermissoes, boolean, callback){
-    var sql ="UPDATE permissoes SET gerirFuncionario = ? WHERE idPermissoes = ?"
-    var values = [boolean, idPermissoes]
-    connection.query(sql, [values], function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
-    })
+        var sql ="UPDATE permissoes SET gerirFuncionario = ? WHERE idPermissoes = ?"
+        var values = [boolean, idPermissoes]
+        connection.query(sql, [values], function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });
+    });
 }
 
 exports.setAgendamentos = function setAgendamentos(idPermissoes, boolean, callback){
-    var sql ="UPDATE permissoes SET validarAgendamentos = ? WHERE idPermissoes = ?"
-    var values = [boolean, idPermissoes]
-    connection.query(sql, [values], function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
-    })
+        var sql ="UPDATE permissoes SET validarAgendamentos = ? WHERE idPermissoes = ?"
+        var values = [boolean, idPermissoes]
+        connection.query(sql, [values], function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });
+    });
 }
 
 exports.setVisita = function setVisita(idPermissoes, boolean, callback){
-    var sql ="UPDATE permissoes SET confirmarVisita = ? WHERE idPermissoes = ?"
-    var values = [boolean, idPermissoes]
-    connection.query(sql, [values], function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
-    })
+        var sql ="UPDATE permissoes SET confirmarVisita = ? WHERE idPermissoes = ?"
+        var values = [boolean, idPermissoes]
+        connection.query(sql, [values], function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });
+    });
 }
 
 exports.setRelatorio = function setRelatorio(idPermissoes, boolean, callback){
-    var sql ="UPDATE permissoes SET gerarRelatorio = ? WHERE idPermissoes = ?"
-    var values = [boolean, idPermissoes]
-    connection.query(sql, [values], function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
-    })
+        var sql ="UPDATE permissoes SET gerarRelatorio = ? WHERE idPermissoes = ?"
+        var values = [boolean, idPermissoes]
+        connection.query(sql, [values], function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });
+    });
 }
 
 
 exports.setAtividade = function setAtividade(idPermissoes, boolean, callback){
-    var sql ="UPDATE permissoes SET inserirAtividade = ? WHERE idPermissoes = ?"
-    var values = [boolean, idPermissoes]
-    connection.query(sql, [values], function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
-    })
+        var sql ="UPDATE permissoes SET inserirAtividade = ? WHERE idPermissoes = ?"
+        var values = [boolean, idPermissoes]
+        connection.query(sql, [values], function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });
+    });
 }
 
 exports.setAtracao = function setAtracao(idPermissoes, boolean, callback){
-    var sql ="UPDATE permissoes SET cadastrarAtracao = ? WHERE idPermissoes = ?"
-    var values = [boolean, idPermissoes]
-    connection.query(sql, [values], function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
-    })
+        var sql ="UPDATE permissoes SET cadastrarAtracao = ? WHERE idPermissoes = ?"
+        var values = [boolean, idPermissoes]
+        connection.query(sql, [values], function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });
+    });
 }
 
 
 exports.remove = function remove(idPermissoes, callback){
-    var sql = "DELETE FROM permissoes WHERE idPermissoes = ?"
-    connection.query(sql, idPermissoes, function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
-    });     
+        var sql = "DELETE FROM permissoes WHERE idPermissoes = ?"
+        connection.query(sql, idPermissoes, function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });     
+    });
 }
--- src/models/Scholarship.js
+++ src/models/Scholarship.js
@@ -1,83 +1,123 @@
-const connection = require("../services/database/connection");
+const pool = require("../services/database/connection");
 
 exports.add = function add(Login, idPessoa, callback){
-    var sql = "INSERT INTO bolisistas (Login, idPessoa, Inativo) VALUES ?";
-    var values = [[Login, idPessoa, 0]];
-    connection.query(sql, [values], function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
-    });    
+        var sql = "INSERT INTO bolisistas (Login, idPessoa, Inativo) VALUES ?";
+        var values = [[Login, idPessoa, 0]];
+        connection.query(sql, [values], function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });    
+    })
 }
 
 exports.getByIdPessoa = function getByIdPessoa(idPessoa, callback){
-    var sql = "SELECT * FROM bolsistas WHERE idPessoa = ?";
-    connection.query(sql, idPessoa, function(err, result){
-        if(err) throw err;
-        callback(result);
-    })    
+    pool.getConnection(function(err, connection){
+        if (err) throw err;
+        var sql = "SELECT * FROM bolsistas WHERE idPessoa = ?";
+        connection.query(sql, idPessoa, function(err, result){
+            if(err) throw err;
+            callback(result);
+            connection.release();
+        });
+    });
 }
 
 exports.getByLogin = function getByLogin(Login, callback){
-    var sql = "SELECT * FROM bolsistas WHERE Login = ?";
-    connection.query(sql, Login, function(err, result){
-        if(err) throw err;
-        callback(result);
-    })     
+    pool.getConnection(function(err, connection){
+        if (err) throw err;
+        var sql = "SELECT * FROM bolsistas WHERE Login = ?";
+        connection.query(sql, Login, function(err, result){
+            if(err) throw err;
+            callback(result);
+            connection.release();
+        });
+    });
 }
 
 exports.getByIdBolsista = function getByIdBolsista(idBolsista, callback){
-    var sql = "SELECT * FROM bolsistas WHERE idBolsista = ?";
-    connection.query(sql, idBolsista, function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
-    });  
+        var sql = "SELECT * FROM bolsistas WHERE idBolsista = ?";
+        connection.query(sql, idBolsista, function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });  
+    });
 }
 
 exports.getAtivos = function getAtivos(callback){
-    var sql = "SELECT * FROM bolsistas WHERE Inativos = 0";
-    connection.query(sql, function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
+        var sql = "SELECT * FROM bolsistas WHERE Inativos = 0";
+        connection.query(sql, function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });
     });   
 }
 
 exports.getInativos = function getInativos(callback){
-    var sql = "SELECT * FROM bolsistas WHERE Inativo = 1"
-    connection.query(sql, function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result)
-    });    
+        var sql = "SELECT * FROM bolsistas WHERE Inativo = 1"
+        connection.query(sql, function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });    
+    });
 }
 
 exports.setAtivo = function setAtivo(idBolsista, callback){
-    var sql = "UPDATE bolsistas SET inativo = 0 WHERE idBolsista = ?"
-    connection.query(sql, idBolsista, function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result)
-    });   
+        var sql = "UPDATE bolsistas SET inativo = 0 WHERE idBolsista = ?"
+        connection.query(sql, idBolsista, function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        });   
+    });
 }
 
 exports.setInativo = function setInativo(idBolsista, callback){
-    var sql = "UPDATE bolsistas SET inativo = 1 WHERE idBolsista = ?";
-    connection.query(sql, idBolsista, function(err, result){
+    pool.getConnection(function(err, connection){
         if (err) throw err;
-        callback(result);
-    })  
+        var sql = "UPDATE bolsistas SET inativo = 1 WHERE idBolsista = ?";
+        connection.query(sql, idBolsista, function(err, result){
+            if (err) throw err;
+            callback(result);
+            connection.release();
+        })  
+    })
 }
 
 exports.setLogin = function setLogin(idBolsista, Login, callback){
-    var sql = "UPDATE bolsistas SET Login = ? WHERE idBolsista = ?";
-    var values = [Login, idBolsista];
-    connection.query(sql, values, function(err, result){
-        if(err) throw err;
-        callback (result);
-    });  
+    pool.getConnection(function(err, connection){
+        if (err) throw err;
+        var sql = "UPDATE bolsistas SET Login = ? WHERE idBolsista = ?";
+        var values = [Login, idBolsista];
+        connection.query(sql, values, function(err, result){
+            if(err) throw err;
+            callback (result);
+            connection.release();
+        });  
+    });
 }
 
 exports.remove = function remove(idBolsista, callback){
-    var sql = "DELETE FROM bolsistas WHERE idBolsista = ?"
-    connection.query(sql, idBolsista, function(err, result){
-        if(err) throw err;
-        callback(result);
-    });    
+    pool.getConnection(function(err, connection){
+        if (err) throw err;
+        var sql = "DELETE FROM bolsistas WHERE idBolsista = ?"
+        connection.query(sql, idBolsista, function(err, result){
+            if(err) throw err;
+            callback(result);
+            connection.release();
+        });    
+    })
 }
