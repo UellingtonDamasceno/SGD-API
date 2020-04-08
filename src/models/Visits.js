18d8cae72b9632c2cde43acd06de917eceeae875
DanielDouradoFsa <60206918+DanielDouradoFsa@users.noreply.github.com>
2020-04-07 23:06:22 -0300

    Update Visits.js

--- src/models/Visits.js
+++ src/models/Visits.js
@@ -1,10 +1,10 @@
 const pool = require("../services/database/connection");
 
-exports.add = function add(idVisitante, numAlunos, Responsavel, status, agendamento, callback){
+exports.add = function add(idVisitante, agendamento, numAlunos, Responsavel, status, serie, observacao, atracoes, hora){
     pool.getConnection(function(err, connection){
         if (err) throw err;
-        var sql = "INSERT INTO visitas (idVisitante, numAlunos, Responsavel, status, agendamento) VALUES  ?"
-        var values  = [[idVisitante, numAlunos, Responsavel, status, agendamento]];
+        var sql = "INSERT INTO visitas (idVisitante, agendamento, numAlunos, Responsavel, status, serie, observacao, atracoes, hora ) VALUES  ?"
+        var values  = [[idVisitante, agendamento, numAlunos, Responsavel, status, serie, observacao, atracoes, hora]];
         connection.query(sql, [values], function(err, result){
             if (err) throw err;
             callback(result);
