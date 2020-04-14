97d37e0a82d23028e3b5afa4dcb68a017d55ee2d
DanielDouradoFsa <60206918+DanielDouradoFsa@users.noreply.github.com>
2020-04-14 13:40:59 -0300

    Pequena mudança

--- src/models/WorkLoad.js
+++ src/models/WorkLoad.js
@@ -17,7 +17,7 @@ exports.add = function add(idBolsista, inicioPeriodo, fimPeriodo,  semana, callb
 exports.getHorario = function getHorario(callback){
     pool.getConnection(function(err, connection){
         if (err) throw err;
-        var sql = 'SELECT semana, inicioPeriodo, fimPeriodo FROM horarioTrabalho';
+        var sql = 'SELECT * FROM horarioTrabalho';
         connection.query(sql, function(err, result){
             if (err) throw err;
             callback(result);
